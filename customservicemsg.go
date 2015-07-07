2f40061a33598021873d0118009caf4ea37d4b04
457381685@qq.com <457381685@qq.com>
2015-07-07 17:36:52 +0800

    add 向用户发送信息。需要验证过到公众号才有权限

---
+++ customservicemsg.go
@@ -0,0 +1,139 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
+)
+
+const (
+	appID                = "wxe9700c43eeb00ac9"
+	appSecret            = "661b23a88162028a228230e4c3c44a7a"
+	accessTokenFetchUrl  = "https://api.weixin.qq.com/cgi-bin/token"
+	customServicePostUrl = "https://api.weixin.qq.com/cgi-bin/message/custom/send"
+)
+
+var openID = "oVNrkwejpgpweCK6FUrQp3Hsql3Q"
+
+type AccessTokenResponse struct {
+	AccessToken string  `json:"access_token"`
+	ExpiresIn   float64 `json:"expires_in"`
+}
+
+type AccessTokenErrorResponse struct {
+	Errcode float64
+	Errmsg  string
+}
+
+// {
+//	"touser":"OPENID",
+//	"msgtype":"text",
+//	"text":
+//	{
+//		"content":"Hello World"
+//	}
+// }
+type CustomServiceMsg struct {
+	ToUser  string         `json:"touser"`
+	MsgType string         `json:"msgtype"`
+	Text    TextMsgContent `json:"text"`
+}
+
+type TextMsgContent struct {
+	Content string `json:"content"`
+}
+
+func fetchAccessToken() (string, float64, error) {
+	requestLine := strings.Join([]string{accessTokenFetchUrl,
+		"?grant_type=client_credential&appid=",
+		appID,
+		"&secret=",
+		appSecret}, "")
+
+	resp, err := http.Get(requestLine)
+	if err != nil || resp.StatusCode != http.StatusOK {
+		return "", 0.0, err
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", 0.0, err
+	}
+
+	fmt.Println(string(body))
+
+	//Json Decoding
+	if bytes.Contains(body, []byte("access_token")) {
+		fmt.Println("return ok")
+		atr := AccessTokenResponse{}
+		err = json.Unmarshal(body, &atr)
+		if err != nil {
+			return "", 0.0, err
+		}
+		return atr.AccessToken, atr.ExpiresIn, nil
+	} else {
+		fmt.Println("return err")
+		ater := AccessTokenErrorResponse{}
+		err = json.Unmarshal(body, &ater)
+		if err != nil {
+			return "", 0.0, err
+		}
+		return "", 0.0, fmt.Errorf("%s", ater.Errmsg)
+	}
+}
+
+func pushCustomMsg(accessToken, toUser, msg string) error {
+	csMsg := &CustomServiceMsg{
+		ToUser:  toUser,
+		MsgType: "text",
+		Text:    TextMsgContent{Content: msg},
+	}
+
+	body, err := json.MarshalIndent(csMsg, " ", "  ")
+	if err != nil {
+		return err
+	}
+	fmt.Println(string(body))
+
+	postReq, err := http.NewRequest("POST",
+		strings.Join([]string{customServicePostUrl, "?access_token=", accessToken}, ""),
+		bytes.NewReader(body))
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("\n", postReq)
+	postReq.Header.Set("Content-Type", "application/json; encoding=utf-8")
+
+	client := &http.Client{}
+	resp, err := client.Do(postReq)
+	if err != nil {
+		return err
+	}
+	resp.Body.Close()
+
+	return nil
+}
+
+func main() {
+	// Fetch access_token
+	accessToken, expiresIn, err := fetchAccessToken()
+	if err != nil {
+		log.Println("Get access_token error:", err)
+		return
+	}
+	fmt.Println(accessToken, expiresIn)
+
+	// Post custom service message
+	msg := "你好" + "\U0001f604"
+	err = pushCustomMsg(accessToken, openID, msg)
+	if err != nil {
+		log.Println("Push custom service message err:", err)
+		return
+	}
+}
